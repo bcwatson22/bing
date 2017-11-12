d70f9a85386b1d640e9e7bb92eae7c82fcad3710
Billy Watson <watson@Billys-MBP.lan>
2017-11-12 20:39:09 +0000

    commission component and editing of static-content data

--- src/app/_data/mock-static-content.ts
+++ src/app/_data/mock-static-content.ts
@@ -7,21 +7,27 @@ export const CONTENT: StaticContent[] = [{
       "Morbi bibendum vulputate mattis. Donec nec lorem felis. Donec ut cursus libero. Vestibulum ut dapibus orci. Donec aliquet felis porttitor ipsum facilisis, sit amet egestas purus faucibus."
     ]
   },
-  "about": {
-    "biography": [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui sque dictum lorem arcu, sit amet vestibulum leo molestie sit  amet. Maecenas quis vehicula massa. Praesent nisl dolor, ulla mcorper id elementum vel, suscipit vel augue. Donec non.",
-      "Mauris ac orci pulvinar bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec pharetra risus ipsum, ut congue lectus semper eget. Aliquam blandit ut nisi eu facilisis.",
-      "Praesent venenatis magna vel libero volutpat, nec fermentum lacus venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis risus quis nibh posuere sodales.",
-      "Maecenas in sodales lacus. Pellentesque pharetra aliquet ligula.",
-      "Eu auctor. Nullam ut sollicitudin dolor. Duis a venenatis nulla. Nunc arcu tortor, Eu auctor. Nullam ut sollicitudin dolor. Duis a venenatis nulla. Nunc arcu tortor."
-    ],
-    "views": [
-      "Pharetra sit amet accumsan ut, porttitor ut metus. Aenean tempor, nunc ac vestibulum accumsan, felis purus ullamcorper velit, cursus rhoncus turpis nulla eget nulla. Ut ac lorem maximus, lobortis felis mattis, egestas eros.",
-      "Nunc malesuada diam at lacus vulputate, non congue velit vehicula. Nullam pellentesque quis orci sit amet imperdiet. Curabitur pharetra sem ac dapibus ullamcorper.",
-      "Posuere felis sit amet lectus facilisis, sed laoreet libero dap ibus. Quisque vel erat sodales, malesuada urna tempus, conse quat felis. Nunc in mi risus. Pellentesque luctus ultricies arcu id.",
-      "Duis mollis dui purus, non mattis purus cursus quis. Nulla facilisi. Sed ac orci gravida, fringilla arcu sit amet, dapibus purus. Sed sagittis, elit et molestie gravida, nisi felis mollis odio, ac dapibus nisi tortor a nulla. Vestibulum tincidunt sodales bibendum. Cras viverra nulla tellus, at auctor orci gravida ac. Proin mollis."
-    ]
-  },
+  "about": [
+    {
+      "title": "Biography",
+      "body": [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui sque dictum lorem arcu, sit amet vestibulum leo molestie sit  amet. Maecenas quis vehicula massa. Praesent nisl dolor, ulla mcorper id elementum vel, suscipit vel augue. Donec non.",
+        "Mauris ac orci pulvinar bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec pharetra risus ipsum, ut congue lectus semper eget. Aliquam blandit ut nisi eu facilisis.",
+        "Praesent venenatis magna vel libero volutpat, nec fermentum lacus venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis risus quis nibh posuere sodales.",
+        "Maecenas in sodales lacus. Pellentesque pharetra aliquet ligula.",
+        "Eu auctor. Nullam ut sollicitudin dolor. Duis a venenatis nulla. Nunc arcu tortor, Eu auctor. Nullam ut sollicitudin dolor. Duis a venenatis nulla. Nunc arcu tortor."
+      ]
+    },
+    {
+      "title": "Views on painting",
+      "body": [
+        "Pharetra sit amet accumsan ut, porttitor ut metus. Aenean tempor, nunc ac vestibulum accumsan, felis purus ullamcorper velit, cursus rhoncus turpis nulla eget nulla. Ut ac lorem maximus, lobortis felis mattis, egestas eros.",
+        "Nunc malesuada diam at lacus vulputate, non congue velit vehicula. Nullam pellentesque quis orci sit amet imperdiet. Curabitur pharetra sem ac dapibus ullamcorper.",
+        "Posuere felis sit amet lectus facilisis, sed laoreet libero dap ibus. Quisque vel erat sodales, malesuada urna tempus, conse quat felis. Nunc in mi risus. Pellentesque luctus ultricies arcu id.",
+        "Duis mollis dui purus, non mattis purus cursus quis. Nulla facilisi. Sed ac orci gravida, fringilla arcu sit amet, dapibus purus. Sed sagittis, elit et molestie gravida, nisi felis mollis odio, ac dapibus nisi tortor a nulla. Vestibulum tincidunt sodales bibendum. Cras viverra nulla tellus, at auctor orci gravida ac. Proin mollis."
+      ]
+    }
+  ],
   "paintings": {
     "intro": [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed rutrum justo, non pulvinar dolor. Fusce hendrerit elit eu felis dapibus cursus. Mauris vel lectus ut ipsum luctus semper. Pra esent non lectus efficitur, egestas nunc vel, venenatis arcu. Orci varius natoque penatibus etma gnis dis parturient montes, nascetur ridiculus mus. Ut ut odio eget enim molestie commodonec at velit. Aenean augue sem, vestibulum id nulla rutrum, consequat iaculis nulla.",
@@ -34,5 +40,89 @@ export const CONTENT: StaticContent[] = [{
       "Morbi bibendum vulputate mattis. Donec nec lorem felis. Donec ut cursus libero. Vestibulum ut dapibus orci. Donec aliquet felis porttitor ipsum facilisis, sit amet egestas purus faucibus."
     ]
   },
-  "commission": {}
+  "commission": {
+    "pricing": {
+      "title": "Pricing of commissioned portraits",
+      "intro": [
+        "Prices vary with size, content and the number of sittings."
+      ],
+      "content": [
+        {
+          "subheader": "Size",
+          "body": [
+            "I am happy to discuss any size from around 8x10 inches upwards. Traditional English middle-sized portraits are 25x30 inches unframed. I am happy to discuss the optimum size in terms of the content, budget and where the picture might hang."
+          ]
+        },
+        {
+          "subheader": "Content",
+          "body": [
+            "A simple head and shoulders will be relatively straightforward. Hands, the full-length figure, a specified setting or personal references may be more involved and more expensive.",
+            "I am keen to discuss any interesting or imaginative ideas: groups, families, settings and objects with a personal ring. I have painted and drawn many children. Traditional oil paint is very flexible!"
+          ]
+        },
+        {
+          "subheader": "Sittings",
+          "body": [
+            "Live sittings are important and may involve travel. I like to meet the sitter if at all possible. I work from photographs when time is limited or when major distances are involved. I live in Sheffield but I am often in London and will travel whenever necessary. Again, please feel free to discuss any preference."
+          ]
+        },
+        {
+          "subheader": "Price Illustration",
+          "body": [
+            "Chalk drawings start at £1500.00. Oil portraits start at around £2500.00 for a head and shoulders. Half-length portraits, including hands, start at around £4000.00. Each portrait will require discussion and an individual quote."
+          ]
+        },
+        {
+          "subheader": "Local Drawings",
+          "body": [
+            "If you live within travelling distance of Sheffield and are willing to attend the artists studio, single sitting pen and wash or graphite sketches can be arranged for as little as £75.00.",
+            "Chalk sketches will cost a little more but both are very suitable for children and groups. Similarly, oil sketches and be arranged for a reduced price. Portraits can be tailored to suit most pockets. Please pick up the phone to discuss any queries."
+          ]
+        },
+        {
+          "subheader": "Frames",
+          "body": [
+            "I am keen to advise on and provide frames, which vary in price and will be additional to the costs above."
+          ]
+        }
+      ]
+    },
+    "contact": {
+      "title": "Contact",
+      "intro": [
+
+      ],
+      "content": [
+        {
+          "subheader": "Post",
+          "body": [
+            "Bing Jones",
+            "14 Whiteley Wood Road",
+            "Sheffield",
+            "South Yorkshire",
+            "S11 7FE",
+            "United Kingdom"
+          ]
+        },
+        {
+          "subheader": "Email",
+          "body": [
+            "bing@bingjones.co.uk"
+          ]
+        },
+        {
+          "subheader": "Phone",
+          "body": [
+            "+44 (0)114 2301003"
+          ]
+        },
+        {
+          "subheader": "Mobile",
+          "body": [
+            "+44 (0)7711 447306"
+          ]
+        }
+      ]
+    }
+  }
 }];
--- src/app/commission/commission.component.ts
+++ src/app/commission/commission.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilsService } from './../_data/services/utils.service';
 
 @Component({
-  selector: 'app-commission',
+  selector: 'commission',
   templateUrl: './commission.component.html',
   styleUrls: ['./commission.component.scss']
 })
+
 export class CommissionComponent implements OnInit {
 
-  constructor() { }
+  private commission;
+
+  constructor(
+    private utils: UtilsService
+  ) {
+
+    utils.getStaticContent();
+
+  }
 
   ngOnInit() {
+
+    this.commission = this.utils.bindData('commission');
+
   }
 
 }
