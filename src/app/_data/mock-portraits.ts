46db30351e125512132cce7907591d02305dbb9c
Billy Watson <watson@Billys-MBP.lan>
2017-11-14 23:02:12 +0000

    paint stroke logo with CSS background-image and PNG fallback, web font loader

--- src/app/_data/mock-portraits.ts
+++ src/app/_data/mock-portraits.ts
@@ -5,7 +5,7 @@ export const PORTRAITS: Portrait[] = [
     id: 'belinda-potter',
     subject: 'Belinda Potter',
     title: 'Miss',
-    material: 'Chalk',
+    material: 'gawd knows',
     description: [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed rutrum justo, non pulvinar dolor. Fusce hendrerit elit eu felis dapibus cursus. Mauris vel lectus ut ipsum luctus semper. Pra esent non lectus efficitur, egestas nunc vel, venenatis arcu. Orci varius natoque penatibus etma gnis dis parturient montes, nascetur ridiculus mus. Ut ut odio eget enim molestie commodonec at velit. Aenean augue sem, vestibulum id nulla rutrum, consequat iaculis nulla.",
         "Morbi bibendum vulputate mattis. Donec nec lorem felis. Donec ut cursus libero. Vestibulum ut dapibus orci. Donec aliquet felis porttitor ipsum facilisis, sit amet egestas purus faucibus."
--- src/app/_data/mock-static-content.ts
+++ src/app/_data/mock-static-content.ts
@@ -3,8 +3,8 @@ import { StaticContent } from './models/static-content';
 export const CONTENT: StaticContent[] = [{
   "home": {
     "intro": [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed rutrum justo, non pulvinar dolor. Fusce hendrerit elit eu felis dapibus cursus. Mauris vel lectus ut ipsum luctus semper. Pra esent non lectus efficitur, egestas nunc vel, venenatis arcu. Orci varius natoque penatibus etma gnis dis parturient montes, nascetur ridiculus mus. Ut ut odio eget enim molestie commodonec at velit. Aenean augue sem, vestibulum id nulla rutrum, consequat iaculis nulla.",
-      "Morbi bibendum vulputate mattis. Donec nec lorem felis. Donec ut cursus libero. Vestibulum ut dapibus orci. Donec aliquet felis porttitor ipsum facilisis, sit amet egestas purus faucibus."
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed rutrum justo, non pulvinar dolor. Fusce hendrerit elit eu felis dapibus cursus. Mauris vel lectus ut ipsum luctus semper. Pra esent non lectus efficitur, egestas nunc vel, venenatis arcu. Orci varius natoque penatibus etma gnis dis parturient montes, nascetur ridiculus mus. Ut ut odio eget enim molestie commodonec at velit. Aenean augue sem, vestibulum id nulla rutrum, consequat iaculis nulla."
+      // "Morbi bibendum vulputate mattis. Donec nec lorem felis. Donec ut cursus libero. Vestibulum ut dapibus orci. Donec aliquet felis porttitor ipsum facilisis, sit amet egestas purus faucibus."
     ]
   },
   "about": [
@@ -30,14 +30,14 @@ export const CONTENT: StaticContent[] = [{
   ],
   "paintings": {
     "intro": [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed rutrum justo, non pulvinar dolor. Fusce hendrerit elit eu felis dapibus cursus. Mauris vel lectus ut ipsum luctus semper. Pra esent non lectus efficitur, egestas nunc vel, venenatis arcu. Orci varius natoque penatibus etma gnis dis parturient montes, nascetur ridiculus mus. Ut ut odio eget enim molestie commodonec at velit. Aenean augue sem, vestibulum id nulla rutrum, consequat iaculis nulla.",
-      "Morbi bibendum vulputate mattis. Donec nec lorem felis. Donec ut cursus libero. Vestibulum ut dapibus orci. Donec aliquet felis porttitor ipsum facilisis, sit amet egestas purus faucibus."
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed rutrum justo, non pulvinar dolor. Fusce hendrerit elit eu felis dapibus cursus. Mauris vel lectus ut ipsum luctus semper. Pra esent non lectus efficitur, egestas nunc vel, venenatis arcu. Orci varius natoque penatibus etma gnis dis parturient montes, nascetur ridiculus mus. Ut ut odio eget enim molestie commodonec at velit. Aenean augue sem, vestibulum id nulla rutrum, consequat iaculis nulla."
+      // "Morbi bibendum vulputate mattis. Donec nec lorem felis. Donec ut cursus libero. Vestibulum ut dapibus orci. Donec aliquet felis porttitor ipsum facilisis, sit amet egestas purus faucibus."
     ]
   },
   "drawings": {
     "intro": [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed rutrum justo, non pulvinar dolor. Fusce hendrerit elit eu felis dapibus cursus. Mauris vel lectus ut ipsum luctus semper. Pra esent non lectus efficitur, egestas nunc vel, venenatis arcu. Orci varius natoque penatibus etma gnis dis parturient montes, nascetur ridiculus mus. Ut ut odio eget enim molestie commodonec at velit. Aenean augue sem, vestibulum id nulla rutrum, consequat iaculis nulla.",
-      "Morbi bibendum vulputate mattis. Donec nec lorem felis. Donec ut cursus libero. Vestibulum ut dapibus orci. Donec aliquet felis porttitor ipsum facilisis, sit amet egestas purus faucibus."
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed rutrum justo, non pulvinar dolor. Fusce hendrerit elit eu felis dapibus cursus. Mauris vel lectus ut ipsum luctus semper. Pra esent non lectus efficitur, egestas nunc vel, venenatis arcu. Orci varius natoque penatibus etma gnis dis parturient montes, nascetur ridiculus mus. Ut ut odio eget enim molestie commodonec at velit. Aenean augue sem, vestibulum id nulla rutrum, consequat iaculis nulla."
+      // "Morbi bibendum vulputate mattis. Donec nec lorem felis. Donec ut cursus libero. Vestibulum ut dapibus orci. Donec aliquet felis porttitor ipsum facilisis, sit amet egestas purus faucibus."
     ]
   },
   "commission": {
